538ba8bd59a09d246bf26e9004748d00c4a56d6c
up1 <somkiat.p@gmail.com>
2022-10-04 16:47:07 +0700

    Service + repository

--- day01/src/main/java/com/example/day01/EmployeeService.java
+++ day01/src/main/java/com/example/day01/EmployeeService.java
@@ -1,10 +1,25 @@
 package com.example.day01;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository repository;
+
     public EmployeeResponse getData(int id) {
-        throw new RuntimeException("Underconstruction");
+        Optional<Employee> result = repository.findById(id);
+        if(result.isPresent()) {
+            Employee employee = result.get();
+            EmployeeResponse response = new EmployeeResponse();
+            response.setId(employee.getId());
+            response.setName(employee.getName());
+            return response;
+        }
+        throw new RuntimeException("Employee not found with id="+ id);
     }
 }
