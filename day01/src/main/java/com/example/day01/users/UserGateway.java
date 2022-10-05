7de339d6e0f28f94b4ffd8afb4681932f5e70c84
up1 <somkiat.p@gmail.com>
2022-10-05 11:50:03 +0700

    Generate response and create a gateway class

---
+++ day01/src/main/java/com/example/day01/users/UserGateway.java
@@ -0,0 +1,13 @@
+package com.example.day01.users;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class UserGateway {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+}
