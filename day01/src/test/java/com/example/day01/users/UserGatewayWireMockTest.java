17e493640c3f98f748f5e849d0f59c3c02146a85
up1 <somkiat.p@gmail.com>
2022-10-05 14:22:59 +0700

    Stub api with wiremock

--- day01/src/test/java/com/example/day01/users/UserGatewayWireMockTest.java
+++ day01/src/test/java/com/example/day01/users/UserGatewayWireMockTest.java
@@ -4,10 +4,19 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.http.MediaType;
+import org.springframework.util.ResourceUtils;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+
 
 @SpringBootTest
 @AutoConfigureWireMock(port = 9999)
@@ -17,11 +26,23 @@ class UserGatewayWireMockTest {
     private UserGateway userGateway;
 
     @Test
-    public void success_case() {
+    public void success_case() throws IOException {
+        stubFor(get(urlPathEqualTo("/users/1"))
+        .willReturn(aResponse()
+                .withBody(read("classpath:userApiResponse.json"))
+                .withHeader(CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .withStatus(200)));
+
+
         Optional<UserResponse> result
                 = userGateway.getUserById(1);
         assertTrue(result.isPresent());
-        assertEquals("Sincere@april.biz", result.get().getEmail());
+        assertEquals("somkiat@xxx.com", result.get().getEmail());
+    }
+
+    public static String read(String filePath) throws IOException {
+        File file = ResourceUtils.getFile(filePath);
+        return new String(Files.readAllBytes(file.toPath()));
     }
 
 }
