108dcbfa05c04500ac6053fdb7e4e891b96e3afe
up1 <somkiat.p@gmail.com>
2022-10-05 12:12:14 +0700

    Integration test with gateway

--- day01/src/main/java/com/example/day01/users/UserGateway.java
+++ day01/src/main/java/com/example/day01/users/UserGateway.java
@@ -2,12 +2,26 @@ package com.example.day01.users;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Optional;
 
 @Component
 public class UserGateway {
 
     @Autowired
     private RestTemplate restTemplate;
+
+    public Optional<UserResponse> getUserById(int id) {
+        String url = "https://jsonplaceholder.typicode.com/users/" + id;
+        try {
+            UserResponse response
+                    = restTemplate.getForObject(url, UserResponse.class);
+            return Optional.of(response);
+        } catch (RestClientException e) {
+            return Optional.empty();
+        }
+    }
 
 }
---
+++ day01/src/test/java/com/example/day01/users/UserGatewayTest.java
@@ -0,0 +1,24 @@
+package com.example.day01.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class UserGatewayTest {
+
+    @Autowired
+    private UserGateway userGateway;
+
+    @Test
+    void getUserById() {
+         Optional<UserResponse> result
+                 = userGateway.getUserById(1);
+         assertTrue(result.isPresent());
+         assertEquals("email", result.get().getEmail());
+    }
+}
