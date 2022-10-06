f7f05e03dc497c01606ed749d445201ae8f62d85
up1 <somkiat.p@gmail.com>
2022-10-06 09:26:53 +0700

    Start workshop with actuator

---
+++ workshop-monitoring/src/main/java/com/example/workshopmonitoring/WorkshopMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.example.workshopmonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopMonitoringApplication.class, args);
+	}
+
+}
---
+++ workshop-monitoring/src/test/java/com/example/workshopmonitoring/WorkshopMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workshopmonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
